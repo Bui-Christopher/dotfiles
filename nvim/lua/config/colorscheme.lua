6116aa9be376b60280955ca66074f80964120e38
Christopher Bui <bui.christopher125@gmail.com>
2023-04-16 16:59:56 -0700

    Refactor and understand neovim configs

---
+++ nvim/lua/config/colorscheme.lua
@@ -0,0 +1,31 @@
+local colorscheme = "gruvbox"
+
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+
+-- Protected call, returns string error but still runs
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
---
+++ nvim/lua/config/plugins.lua
@@ -0,0 +1,93 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim and syncs plugins whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+
+    -- General
+    use "wbthomason/packer.nvim"                  -- Have packer manage itself
+    use "nvim-lua/plenary.nvim"                   -- Useful lua functions used by lots of plugins
+  
+    -- Colorscheme
+    use { "ellisonleao/gruvbox.nvim" }
+  
+    -- Completions
+    use "hrsh7th/nvim-cmp"                      -- The completion plugin
+    use "hrsh7th/cmp-buffer"                    -- buffer completions
+    use "hrsh7th/cmp-path"                      -- path completions
+    use "hrsh7th/cmp-cmdline"                   -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip"              -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp"                  -- LSP completions
+
+    use "windwp/nvim-autopairs"                 -- Matching brackets
+
+    -- Comments
+    use "numToStr/Comment.nvim"                 -- Easy comments
+
+    -- Snippets
+    use "L3MON4D3/LuaSnip"                      -- Snippet Engine
+    use "rafamadriz/friendly-snippets"          -- a bunch of snippets to use
+  
+    -- LSP
+    use "neovim/nvim-lspconfig"                 -- enable LSP
+    use "williamboman/mason.nvim"               -- simple to use language server installer
+    use "williamboman/mason-lspconfig.nvim"     -- required for mason.nvim 
+    use "j-hui/fidget.nvim"                     -- UI for lsp progress
+  
+    use 'simrat39/rust-tools.nvim'              -- Rust
+  
+    -- Syntax Tree 
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+  
+    -- Fuzzy Finder
+    use "nvim-telescope/telescope.nvim"
+    
+    -- Git
+    use "lewis6991/gitsigns.nvim"
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+end)
