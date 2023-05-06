92ff3cbd07e604868a540dc8adc9d689964a161d
Christopher Bui <bui.christopher125@gmail.com>
2023-05-05 20:30:13 -0700

    Fix rust-tools on_attach

--- nvim/lua/config/lsp/rust.lua
+++ nvim/lua/config/lsp/rust.lua
@@ -15,26 +15,37 @@ require("rust-tools").setup({
     },
 
     server = {
+        on_attach = on_attach,
+        -- on_attach = function(_, bufnr)
+        --     -- Hover actions
+        --     vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+        --     -- Code action groups
+        --     vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        -- end,
+
         settings = {
-        ["rust-analyzer"] = {
-            imports = {
-                granularity = {
-                    group = "module",
+            ["rust-analyzer"] = {
+                imports = {
+                   granularity = {
+                       group = "module",
+                   },
+                   prefix = "self",
+               },
+               cargo = {
+                   buildScripts = {
+                       enable = true,
+                   },
                 },
-                prefix = "self",
-            },
-            cargo = {
-                buildScripts = {
-                    enable = true,
+                procMacro = {
+                   enable = true,
                 },
-             },
-             procMacro = {
-                enable = true,
-             },
-             checkOnSave = {
-                 command = "clippy",
-             }
-         }
-     }
- },
+                checkOnSave = {
+                    command = "clippy",
+                }
+                -- rustfmt = {
+                --     extraArgs = { "+nightly" },
+                -- }
+            }
+        }
+    },
 })
