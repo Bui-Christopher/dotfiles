bcac82d640c2beec78e4b97214cad126f85437df
Chris <cbui@crownsterling.io>
2023-05-09 19:17:16 -0700

    Add clarifying comments and make pkg to pacman

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -48,7 +48,7 @@ chown -R "$username:$username" "$home/Downloads"
 
 # Package Manager/Packages
 sudo pacman -Syu
-sudo pacman -S --noconfirm blueberry chromium clang cmake curl discord docker docker-compose fakeroot feh gcc gzip iproute2 nim unzip vlc wget zsh
+sudo pacman -S --noconfirm blueberry chromium clang cmake curl discord docker docker-compose fakeroot feh gcc gzip iproute2 make nim unzip vlc wget zsh
 
 ## AUR
 git clone https://aur.archlinux.org/yay.git
@@ -58,6 +58,10 @@ sudo -u "$username" makepkg -sri --noconfirm
 cd ..
 rm -rf yay
 
+## Docker
+sudo systemctl enable docker
+# TODO: Add user to not require sudo
+
 # Set up configs
 cd "$config"
 git submodule update --init --recursive
@@ -65,7 +69,6 @@ ln -sf "$config/git/.gitconfig" "$home/.gitconfig"
 cp "$config/zsh/.zshrc_home" "$home/.zshrc"
 chsh -s /bin/zsh
 
-sudo systemctl enable docker
 
 # Rust
 curl -s https://sh.rustup.rs > rust.sh
@@ -87,12 +90,14 @@ sudo mv "$nitch_dir/nitch" "/usr/local/bin/nitch"
 sudo pacman -S --noconfirm xorg-server xorg-xinit xsel i3-wm dmenu xorg-xrandr arandr polybar lightdm lightdm-gtk-greeter
 sudo systemctl enable bluetooth.service
 sudo systemctl enable lightdm
+
+# Have bluetooth.service start prior to lightdm.service
 # systemctl edit --full lightdm.service << EOF
 # [Unit]
 # After=bluetooth.service
 # EOF
 
 cd $config
-git remote add origin git@github.com:Bui-Christopher/dotfiles.git 
+git remote set-url origin git@github.com:Bui-Christopher/dotfiles.git 
 
-reboot
+# reboot
