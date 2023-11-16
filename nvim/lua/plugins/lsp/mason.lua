6059c1e363a36d45c03ca6f6f856f98af6daad47
Chris <christopherphongbui@gmail.com>
2023-11-16 04:32:29 -0800

    Fix rust-tools with LSP and refactor unused code

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,18 +1,13 @@
 return {
     "williamboman/mason.nvim",
-	event = "BufReadPre",
+    event = "BufReadPre",
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
-        -- "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
-        -- import mason
         local mason = require("mason")
-        -- import mason-lspconfig
         local mason_lspconfig = require("mason-lspconfig")
-        -- local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
         mason.setup({
             ui = {
                 icons = {
@@ -24,24 +19,12 @@ return {
         })
 
         mason_lspconfig.setup({
-            -- list of servers for mason to install
             ensure_installed = {
                 "lua_ls",
                 "pyright",
                 "rust_analyzer"
             },
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
+            automatic_installation = true,
         })
-
-        -- mason_tool_installer.setup({
-        --   ensure_installed = {
-        --     "prettier", -- prettier formatter
-        --     "stylua", -- lua formatter
-        --     "isort", -- python formatter
-        --     "black", -- python formatter
-        --     "pylint", -- python linter
-        --   },
-        -- })
     end,
 }
