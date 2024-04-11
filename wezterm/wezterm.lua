d0b7105221f87dd17f06d8215a8d8196a9e90554
Chris <christopherphongbui@gmail.com>
2024-04-11 15:24:49 -0700

    Fix hyprland workspace default and add wezterm font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 config.enable_wayland = false
 config.color_scheme = 'Windows High Contrast (base16)'
 config.font = wezterm.font 'MesloLGS NF'
+config.font_size = 14.5
 
 return config
