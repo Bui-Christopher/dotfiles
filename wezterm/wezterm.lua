06e03587c4e16889fef505dbebb03314ef3e685e
Chris Bui <christopherphongbui@gmail.com>
2024-09-09 22:43:51 -0700

    Fix hyprland default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,18 +5,18 @@ config = {
     enable_wayland = false,
     color_scheme = 'Windows High Contrast (base16)',
     font = wezterm.font 'MesloLGS NF',
-    font_size = 14.5,
+    font_size = 15,
     keys = {
-      {
-        key = "t",
-        mods = "CTRL",
-        action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-      },
-      {
-        key = "w",
-        mods = "CTRL",
-        action = wezterm.action.CloseCurrentTab({ confirm = true }),
-      },
+        {
+            key = "t",
+            mods = "CTRL",
+            action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+        },
+        {
+            key = "w",
+            mods = "CTRL",
+            action = wezterm.action.CloseCurrentTab({ confirm = true }),
+        },
     }
 }
 
