19054364c84290db1277db99353500561692cfd7
Chris Bui <christopherphongbui@gmail.com>
2024-11-05 20:22:24 -0800

    Reset gitignore cache

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 # Terminate already running bar instances
 #
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 # polybar-msg cmd quit
 #
 # Otherwise you can use the nuclear option:
